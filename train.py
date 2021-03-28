9e8d3210f64e641f29f38ca775211cc9a29b1b2c
mallepallihimasagar <mallepallihimasagar@gmail.com>
2021-03-28 21:05:01 +0000

    removed errors

--- train.py
+++ train.py
@@ -19,7 +19,6 @@ from models import U_Net
 from models import ResAttU_Net
 from tqdm import tqdm
 from loss_functions import DiceLoss
-
 from check import pixel_accuracy,IoU,dice_metric
 
 #set random seed
@@ -30,8 +29,8 @@ np.random.seed(0)
 BATCH_SIZE = 4
 LEARNING_RATE = 1e-4
 VALID_SPLIT = 0.2
-NUM_EPOCHS = 50
-PATIENCE = 10
+NUM_EPOCHS = 0
+PATIENCE = 15
 ALPHA = 0.5
 INPUT_CHANNELS=3
 OUTPUT_CHANNELS=4  
@@ -73,7 +72,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 # step_size = 2, after every 2 epoch, new_lr = lr*gamma 
 
 # gamma = decaying factor
-scheduler = StepLR(optimizer, step_size=20, gamma=0.1)
+scheduler = StepLR(optimizer, step_size=40, gamma=0.1)
 
 def train_model(
         model=model,
@@ -88,7 +87,7 @@ def train_model(
     
     best_model_wts = copy.deepcopy(model.state_dict())
     best_loss = 1e10
-    
+    patience_test =0
     for epoch in range(num_epochs):
         
         #training phase
@@ -181,6 +180,12 @@ def train_model(
             print("saving best model")
             best_loss = avg_loss
             best_model_wts = copy.deepcopy(model.state_dict())
+            patience_test=0
+        else :
+            patience_test +=1
+        if patience_test >PATIENCE:
+            print("out of patience, breaking the training loop")
+            break
             
     model.load_state_dict(best_model_wts)
     return model
@@ -196,9 +201,9 @@ final_model = train_model(
         criteria = CrossEntropy_loss,
         optimizer=optimizer,
         device=device)
-model_path = "/content/nissl_project_cs19m036/trained_models/unet.pt"
+model_path = "/content/nissl_project_cs19m036/trained_models/unet_CEloss_unweighted.pt"
 
-print('Training completed , saving model weights to {model_path}')
+print(f'Training completed , saving model weights to {model_path}')
 try:
     torch.save(model.state_dict(), model_path)
     print("model weights saved succesfully")
@@ -207,38 +212,47 @@ except:
 
 
 #testing a sample
-inputs,masks = next(iter(val_loader))
-input_org = inputs
-masks_org = masks
-inputs = torch.from_numpy(inputs/255).to(device).unsqueeze(dim=0)
-targets = torch.from_numyp(masks).to(torch.long).to(device).unsqueeze(dim=0)
-#onehot encoding with [batchsize, num_classes, Width , Height]
-onehot_masks = torch.nn.functional.one_hot(target).permute(0,3,1,2).to(device)
-                
-#optimizer.zero_grad()
-outputs = model(inputs)
-loss = criteria(outputs,targets)
-                
-                
-#calculate metrics
-avg_loss = (avg_loss*index+loss.item())/(index+1)
-pixel_acc = (pixel_acc*index+pixel_accuracy(outputs.cpu(),targets.cpu()))/(index+1)
-dice_coef = (dice_coef*index+dice_metric(outputs.cpu(),onehot_masks.cpu()))/(index+1)
-IoU_cell1 = (IoU_cell1*index+IoU(outputs.cpu(),onehot_masks.cpu(),cell=1))/(index+1)
-IoU_cell2 = (IoU_cell2*index+IoU(outputs.cpu(),onehot_masks.cpu(),cell=2))/(index+1)
-IoU_cell3 = (IoU_cell3*index+IoU(outputs.cpu(),onehot_masks.cpu(),cell=3))/(index+1)
-    
-output = output.cpu().squeeze(dim=0)
-output = torch.nn.Softmax(dim=0)(output)
-output_mask = torch.argmax(output, dim=0)
-cv2.imwrite('output.tif',output_mask.numpy().astype(np.uint8))
-cv2.imwrite('input.png',input_org)
-cv2.imwrite('target.tif',masks_org)
+with torch.no_grad():
+  inputs,masks = next(iter(val_loader))
+  print(inputs.shape,masks.shape)
+  inputs = inputs.numpy()[0]
+  masks = masks.numpy()[0]
+  input_org = inputs
+  masks_org = masks
+  inputs = torch.from_numpy(inputs/255).permute(2,0,1).to(device).unsqueeze(dim=0)
+  targets = torch.from_numpy(masks).to(torch.long).to(device).unsqueeze(dim=0)
 
-print("avg_loss = ",avg_loss)
-print("pixel accuracy = ",pixel_acc)
-print("dice_coef = ",dice_coef)
-print("IOU of cell 1,2,3 = ",IoU_cell1,IoU_cell2,IoU_cell3)
+  inputs = inputs.type(torch.float)
+  targets = targets.type(torch.long)
+  #onehot encoding with [batchsize, num_classes, Width , Height]
+  onehot_masks = torch.nn.functional.one_hot(targets)
+  print(onehot_masks.shape)
+  onehot_masks = onehot_masks.permute(0,3,1,2).to(device)
+                  
+  #optimizer.zero_grad()
+  outputs = final_model(inputs)
+  loss = CrossEntropy_loss(outputs,targets)
+                  
+                  
+  #calculate metrics
+  avg_loss = loss.item()
+  pixel_acc = pixel_accuracy(outputs.detach().cpu(),targets.detach().cpu())
+  dice_coef = dice_metric(outputs.detach().cpu(),onehot_masks.detach().cpu()).item()
+  IoU_cell1 = IoU(outputs.detach().cpu(),onehot_masks.detach().cpu(),cell=1)
+  IoU_cell2 = IoU(outputs.detach().cpu(),onehot_masks.detach().cpu(),cell=2)
+  IoU_cell3 = IoU(outputs.detach().cpu(),onehot_masks.detach().cpu(),cell=3)
+      
+  outputs = outputs.detach().squeeze(dim=0)
+  outputs = torch.nn.Softmax(dim=0)(outputs)
+  output_mask = torch.argmax(outputs, dim=0)
+  cv2.imwrite('output.tif',output_mask.cpu().numpy().astype(np.uint8))
+  cv2.imwrite('input.png',input_org)
+  cv2.imwrite('target.tif',masks_org)
+
+  print("avg_loss = ",avg_loss)
+  print("pixel accuracy = ",pixel_acc)
+  print("dice_coef = ",dice_coef)
+  print("IOU of cell 1,2,3 = ",IoU_cell1,IoU_cell2,IoU_cell3)
 
 
                 
